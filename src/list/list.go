59daa9d970611a296834a7128d2054e1f0d4a2b8
Philipp Larionov <phillarov@gmail.com>
2019-08-05 19:11:26 +0300

    1st commit

---
+++ src/list/list.go
@@ -0,0 +1,45 @@
+package list
+
+import (
+	"fmt"
+)
+
+type Node struct {
+	Data interface {}
+	next *Node
+	prev *Node
+}
+
+type List struct {
+	head *Node
+	length int
+}
+
+func (l *List) Show() {
+	if l.length != 0 {
+		tmp := l.head
+		// List.Append(n) work as stack append
+		// so first find the last node
+		for i:=1; i<l.length; i++ {
+			tmp = l.head.next
+		}
+		// second print nodes from the end
+		for i:=1; i<l.length; i++ {
+			fmt.Println(tmp)
+			tmp = tmp.prev
+		}
+	}
+}
+
+func (l *List) Append(node *Node) {
+	node.next = l.head
+	if l.head != nil {
+		l.head.prev = node
+	}
+	l.head = node
+	l.length++
+}
+
+// func (l *List) Remove(node *Node) {
+
+// }
